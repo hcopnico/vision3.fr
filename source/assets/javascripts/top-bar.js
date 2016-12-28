92e09e56ccb4a9a01b948f66fde09492c3029ddd
David Leuliette <dleuliette@gmail.com>
2016-12-28 17:57:50 +0100

    fix(top-bar): update behavior for mobile

--- source/assets/javascripts/top-bar.js
+++ source/assets/javascripts/top-bar.js
@@ -2,8 +2,13 @@ function mobileMenu () {
   var button = $('#js-top-bar--button');
   var target = $('#js-top-bar--links');
 
+  var textOpen = $('#js-open');
+  var textClose = $('#js-close');
+
   button.click(function() {
     target.fadeToggle(300);
+    textOpen.toggle();
+    textClose.toggle();
   });
 
 }
